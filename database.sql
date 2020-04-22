200c85c8d921c068059e0b29d977cb61145bf9f8
SumanthEadula <59619965+SumanthEadula@users.noreply.github.com>
2020-04-21 21:37:21 -0400

    random

--- database.sql
+++
@@ -1,3 +0,0 @@
-create database Project2;
-use database Project2;
-create table Registration(Email varchar(250),Fullaname varchar(250),DOB varchar(250),StreetNo varchar(250),StreetName varchar(250),City varchar(250),Province varchar(250),Country varchar(250),PostalCode varchar(250));
