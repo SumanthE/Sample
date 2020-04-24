7641a91737914a4757e88cc343f2904a1aec0072
SumanthEadula <59619965+SumanthEadula@users.noreply.github.com>
2020-04-23 23:22:12 -0400

    Updated

---
+++ database.sql
@@ -0,0 +1,18 @@
+create database Project2;
+use Project2;
+create table UserDetails(
+    email varchar(255),
+    password varchar(255)
+    );
+create table Registration(
+    Email varchar(250),
+    Fullname varchar(250),
+    DOB varchar(250),
+    StreetNo varchar(250),
+    StreetName varchar(250),
+    City varchar(250),
+    Province varchar(250),
+    Country varchar(250),
+    PostalCode varchar(250)
+
+);
